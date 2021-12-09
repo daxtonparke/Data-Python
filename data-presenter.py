54b35ec54631881e7352c45ef38a12fa1c3ce558
daxtonparke <daxtonparke@gmail.com>
2021-12-09 14:51:01 -0700

    complete

--- data-presenter.py
+++ data-presenter.py
@@ -3,20 +3,20 @@ cupcake_invoices = open('CupcakeInvoices.csv')
 for cup in cupcake_invoices:
     print(cup)
     
-for order in cupcake_invoices:
-  splitorder = order.rstrip('\n').split(',')
-  print(splitorder[2])
+# for order in cupcake_invoices:
+#   splitorder = order.rstrip('\n').split(',')
+#   print(splitorder[2])
 
-for order in cupcake_invoices:
-  splitorder = order.rstrip('\n').split(',')
-  print(float(splitorder[3])*float(splitorder[4]))
+# for order in cupcake_invoices:
+#   splitorder = order.rstrip('\n').split(',')
+#   print(round(float(splitorder[3])*float(splitorder[4]),2))
 
-total = 0
-running_total = 0
-for order in cupcake_invoices:
-  splitorder = order.rstrip('\n').split(',')
-  total = (float(splitorder[3])*float(splitorder[4]))
-  running_total += total
-print(running_total)
+# total = 0
+# running_total = 0
+# for order in cupcake_invoices:
+#   splitorder = order.rstrip('\n').split(',')
+#   total = round(float(splitorder[3])*float(splitorder[4]), 2)
+#   running_total += total
+# print(running_total)
 
 cupcake_invoices.close()
